a887566d2a9dfffc43a805b336330fa8feaa54e0
servletabc <servletabc@gmail.com>
2015-11-06 17:29:24 +0800

    addition

--- ngx_http_access_dynamic_module.c
+++ ngx_http_access_dynamic_module.c
@@ -38,12 +38,15 @@ static char *ngx_http_access_dynamic_push_command(ngx_conf_t *cf, ngx_command_t 
 static ngx_int_t ngx_http_access_dynamic_push_handler(ngx_http_request_t *r);
 static ngx_int_t ngx_http_access_dynamic_init(ngx_conf_t *cf);
 static ngx_int_t ngx_http_access_dynamic_handler(ngx_http_request_t *r);
-static char *ngx_http_access_dynamic_get_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_access_dynamic_get_handler(ngx_http_request_t *r);
+static char *ngx_http_access_dynamic_exist_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_access_dynamic_exist_handler(ngx_http_request_t *r);
 static char *ngx_http_access_dynamic_enable(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_access_dynamic_init_zone (ngx_shm_zone_t *shm_zone, void *data);
 void ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r);
 static ngx_int_t insert_into_dict(ngx_http_access_dynamic_ctx_t  *ctx, ngx_str_t ipaddr,ngx_cidr_t *cidr);
+static ngx_int_t search_from_dict(ngx_http_access_dynamic_ctx_t  *ctx, ngx_str_t *ipaddr);
+static char *ngx_http_access_dynamic_del_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_access_dynamic_del_handler(ngx_http_request_t *r);
 
 static ngx_command_t ngx_http_access_dynamic_commands[] = {
 	{
@@ -63,9 +66,17 @@ static ngx_command_t ngx_http_access_dynamic_commands[] = {
 			NULL
 	},
 	{
-			ngx_string("access_dynamic_get"),
+			ngx_string("access_dynamic_exist"),
 			NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
-			ngx_http_access_dynamic_get_command,
+			ngx_http_access_dynamic_exist_command,
+			NGX_HTTP_LOC_CONF_OFFSET,
+			0,
+			NULL
+	},
+	{
+			ngx_string("access_dynamic_del"),
+			NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+			ngx_http_access_dynamic_del_command,
 			NGX_HTTP_LOC_CONF_OFFSET,
 			0,
 			NULL
@@ -231,7 +242,7 @@ void
 ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r){
 	ngx_int_t rc;
 	u_char *body;
-	size_t body_size = 0;
+	size_t body_size;
 	ngx_chain_t *ch;
 	ngx_array_t *ipstr_arr;
 	ngx_array_t *ipstr_invalid_arr;
@@ -239,22 +250,33 @@ ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r){
 	ngx_uint_t loop;
 	ngx_http_access_dynamic_main_conf_t *main_conf;
 	ngx_http_access_dynamic_ctx_t *ctx;
+	ngx_chain_t *out;
+	ngx_buf_t *b;
 
-	main_conf = ngx_http_get_module_loc_conf(r, ngx_http_access_dynamic_module);
+	main_conf = ngx_http_get_module_main_conf(r, ngx_http_access_dynamic_module);
 	ctx = main_conf->shm_zone->data;
 
-	/*split ip addr(ipv4 support only now) from request body and build ngx_array_t*/
+	/*1.split ip addr(ipv4 support only now) from request body and build ngx_array_t*/
 	ngx_http_request_body_t *rb = r->request_body;
 	if (rb == NULL) {
 		ngx_http_finalize_request(r, NGX_HTTP_INTERNAL_SERVER_ERROR);
 		return;
 	}
+	body_size = 0;
 	for (ch = rb->bufs; ch; ch = ch->next) {
 		ngx_buf_t *b = ch->buf;
 	    ngx_int_t len = b->last - b->pos;
 	    body_size += len;
 	}
+	if(body_size<=0){
+		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,  "body_size less than 0");
+		goto GO_DONE;
+	}
 	body = ngx_pcalloc(r->pool,body_size);
+	if(body==NULL){
+		ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,  "body is NULL");
+		goto GO_DONE;
+	}
 	last_cpy = body;
 	for (ch = rb->bufs; ch; ch = ch->next) {
 			ngx_buf_t *b = ch->buf;
@@ -263,11 +285,10 @@ ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r){
 	}
 	ipstr_arr = ngx_array_create(r->pool, PUSH_BATCH_SIZE, sizeof(ngx_str_t));
 	ipstr_invalid_arr = ngx_array_create(r->pool, PUSH_BATCH_SIZE, sizeof(ngx_str_t));
-	if(body && body_size>0){
-	    	u_char *split_pos_last = body;
-	    	char separator = '&';
-	    	u_char *split_pos;
-	    	for(loop=0;loop<PUSH_BATCH_SIZE;loop++){
+   	u_char *split_pos_last = body;
+	char separator = '&';
+	u_char *split_pos;
+	for(loop=0;loop<PUSH_BATCH_SIZE;loop++){
 	    		split_pos = ngx_strlchr(split_pos_last,body+body_size,separator);
 	    		if(split_pos){
 	    			size_t len = split_pos - split_pos_last;
@@ -282,14 +303,12 @@ ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r){
 	    			ipstr->len = len;
 	    			break;
 	    		}
-	    	}
+	  }
 
-	    	/*valid ip addr and insert into dic*/
-	    	ngx_str_t *value=(ngx_str_t *)ipstr_arr->elts;
-	    	for(loop=0;loop<ipstr_arr->nelts;loop++){
+	  /*2.valid ip addr and insert into dic, invalid into ipstr_invalid_arr*/
+	  ngx_str_t *value=(ngx_str_t *)ipstr_arr->elts;
+	  for(loop=0;loop<ipstr_arr->nelts;loop++){
 	    		ngx_str_t ipstr = value[loop];
-	    //		ipstr.data[ipstr.len-1] = '\0';
-	    //		ngx_log_error(NGX_LOG_ERR,r->connection->log,0,"ipstr:%s",ipstr.data);
 	    		ngx_cidr_t cidr;
 	    		ngx_memzero(&cidr, sizeof(ngx_cidr_t));
 	    		rc = ngx_ptocidr(&ipstr, &cidr);
@@ -312,77 +331,78 @@ ngx_http_access_dynamic_push_post_handler(ngx_http_request_t *r){
 	    			insert_into_dict(ctx,ipstr,&cidr);
 	    		}
 
-	    	}
+	 }
 
-	    	/* show resule */
-	    	ngx_str_set(&r->headers_out.content_type,"text/plain");
-	    	ngx_chain_t *out = ngx_alloc_chain_link(r->pool);
-	    	if(ipstr_invalid_arr->nelts>0){
+	 /*3.print result */
+	 ngx_str_set(&r->headers_out.content_type,"text/plain");
+	 out = ngx_alloc_chain_link(r->pool);
+	 if(out == NULL){
+	   	return ;
+	 }
+	 if(ipstr_invalid_arr->nelts>0){
 	    		ngx_int_t invalid_size = 0;
 	    		ngx_str_t *value=(ngx_str_t *)ipstr_invalid_arr->elts;
 	    		for(loop=0;loop<ipstr_invalid_arr->nelts;loop++){
 	    			ngx_str_t invalid_ipstr = value[loop];
 	    			invalid_size += invalid_ipstr.len;
 	    		}
-	    		invalid_size = sizeof("invalid ips:") + invalid_size + ipstr_invalid_arr->nelts * sizeof(',') + sizeof('\0');
-	    		ngx_buf_t *b = ngx_create_temp_buf(r->pool,invalid_size);
+	    		invalid_size = sizeof("invalid ips:") + invalid_size + ipstr_invalid_arr->nelts * sizeof(',') + sizeof("\r\n");
+	    		b = ngx_create_temp_buf(r->pool,invalid_size);
 	    		if(b == NULL){
+	    			ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,  "error happend when alloc memory(ngx_create_temp_buf)");
 	    			goto GO_DONE;
 	    		}
-	    		u_char *invalid_buf = ngx_pcalloc(r->pool,invalid_size);
+	    		u_char *invalid_buf = b->pos;//ngx_pcalloc(r->pool,invalid_size);
 	    		u_char *invalid_buf_pos;
-	    		if(invalid_buf == NULL){
-	    			 goto GO_DONE;
-	    		}
 	    		invalid_buf_pos = ngx_sprintf(invalid_buf,"invalid ips:");
 	    		for(loop=0;loop<ipstr_invalid_arr->nelts;loop++){
 	    			ngx_str_t invalid_ipstr = value[loop];
-	    			invalid_buf_pos = ngx_cpystrn(invalid_buf_pos,invalid_ipstr.data,invalid_ipstr.len);
-	    			invalid_buf_pos = ngx_sprintf(invalid_buf_pos,",");
+	    			invalid_buf_pos = ngx_snprintf(invalid_buf_pos,invalid_ipstr.len,"%s",invalid_ipstr.data);
+	    			invalid_buf_pos++;
+	    			invalid_buf_pos = ngx_snprintf(invalid_buf_pos,1,",");
+	    			invalid_buf_pos++;
 	    		}
-	    		*invalid_buf_pos = '\0';
+	    		invalid_buf_pos = ngx_snprintf(invalid_buf_pos,sizeof("\r\n"),"\r\n");
 
-	    		out->buf = b;
-	    		out->next = NULL;
-	    		b->last = invalid_buf;
+	    		b->last = invalid_buf_pos;
 	    		b->last_buf = 1;
-	    	}else{
+	}else{
 	    		size_t size = sizeof("ok");
-	    		ngx_buf_t *b = ngx_create_temp_buf(r->pool,size);
-	    		out->buf = b;
-	    		out->next = NULL;
+	    		b = ngx_create_temp_buf(r->pool,size);
 	    		b->last = ngx_sprintf(b->last,"ok");
 	    		b->last_buf = 1;
-	    	}
-
-	    	r->headers_out.status = NGX_HTTP_OK;
-	    	rc = ngx_http_send_header(r);
-	    	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
-	    	    return;
-	    	}
-	    	ngx_http_output_filter(r, out);
-
-	}else{
-
-		GO_DONE:
-		ngx_str_set(&r->headers_out.content_type,"text/plain");
-		size_t size = sizeof("internal error:\r\n");
-		ngx_buf_t *b = ngx_create_temp_buf(r->pool,size);
-		ngx_chain_t *out = ngx_alloc_chain_link(r->pool);
-		if(b == NULL || out == NULL){
-			return ;
-		}
-		out->buf = b;
-		out->next = NULL;
-		b->last = ngx_sprintf(b->last,"internal error\r\n");
-		b->last_buf = 1;
-		r->headers_out.status = NGX_HTTP_SERVICE_UNAVAILABLE;
-		rc = ngx_http_send_header(r);
-		if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
-			return;
-		}
-		ngx_http_output_filter(r, out);
 	}
+
+	out->buf = b;
+	out->next = NULL;
+	r->headers_out.status = NGX_HTTP_OK;
+	rc = ngx_http_send_header(r);
+	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
+	    return;
+	}
+	ngx_http_output_filter(r, out);
+	return;
+
+
+GO_DONE:
+	ngx_str_set(&r->headers_out.content_type,"text/plain");
+	size_t size = sizeof("internal error:\r\n");
+	b = ngx_create_temp_buf(r->pool,size);
+	out = ngx_alloc_chain_link(r->pool);
+	if(b == NULL || out == NULL){
+		return ;
+	}
+	out->buf = b;
+	out->next = NULL;
+	b->last = ngx_sprintf(b->last,"internal error\r\n");
+	b->last_buf = 1;
+	r->headers_out.status = NGX_HTTP_SERVICE_UNAVAILABLE;
+	rc = ngx_http_send_header(r);
+	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
+		return;
+	}
+	ngx_http_output_filter(r, out);
+	return;
 }
 
 static ngx_int_t
@@ -420,17 +440,45 @@ insert_into_dict(ngx_http_access_dynamic_ctx_t  *ctx, ngx_str_t ipaddr,ngx_cidr_
     return last_used_bucket;
 }
 
+static ngx_int_t
+search_from_dict(ngx_http_access_dynamic_ctx_t  *ctx, ngx_str_t *ipaddr){
+	ngx_http_access_dynamic_shctx_t *shm;
+    shm= ctx->shm;
+    ngx_uint_t key;
+    key = ngx_hash_key_lc((u_char *)ipaddr->data, ipaddr->len) % DICT_CAPACITY;
+    ngx_int_t idx = shm->bucket_idx[key];
+    if (idx >= 0) {
+        while (idx >= 0 && ngx_strncasecmp((u_char *)&shm->bucket_arr[idx].addr_text, ipaddr->data,ipaddr->len)) {
+            idx = shm->bucket_arr[idx].next;
+        }
+        if (idx >= 0) {
+            return idx;
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;
+    }
+}
+
 static char *
-ngx_http_access_dynamic_get_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf){
+ngx_http_access_dynamic_exist_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf){
 	ngx_http_core_loc_conf_t  *clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-	clcf->handler = ngx_http_access_dynamic_get_handler;
+	clcf->handler = ngx_http_access_dynamic_exist_handler;
 	return NGX_CONF_OK;
 }
 
 static ngx_int_t
-ngx_http_access_dynamic_get_handler(ngx_http_request_t *r){
+ngx_http_access_dynamic_exist_handler(ngx_http_request_t *r){
 	ngx_int_t rc;
-	if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD)))	{
+	ngx_str_t arg_ip_value;
+	char *arg_ip_name = "ip=";
+	char separator = '&';
+	u_char *arg_ip_value_start,*arg_ip_value_end;
+	ngx_http_access_dynamic_main_conf_t *main_conf;
+	ngx_http_access_dynamic_ctx_t *ctx;
+
+	if (!(r->method & (NGX_HTTP_GET)))	{
 	    return NGX_HTTP_NOT_ALLOWED;
 	}
 	rc = ngx_http_discard_request_body(r);
@@ -438,38 +486,93 @@ ngx_http_access_dynamic_get_handler(ngx_http_request_t *r){
 	    return rc;
 	}
 	ngx_str_set(&r->headers_out.content_type,"text/plain");
-	if(r->method == NGX_HTTP_HEAD)	{
-		r->headers_out.status = NGX_HTTP_OK;
-		rc = ngx_http_send_header(r);
-		if(rc == NGX_ERROR || rc > NGX_OK || r->header_only)		{
-			return rc;
-		}
-	}
+	main_conf = ngx_http_get_module_main_conf(r,ngx_http_access_dynamic_module);
+	ctx = (ngx_http_access_dynamic_ctx_t *)main_conf->shm_zone->data;
+	arg_ip_value_start = ngx_strnstr((u_char *)r->args.data,arg_ip_name,r->args.len);
+	arg_ip_value_start += sizeof("ip=");
+	arg_ip_value_end = ngx_strlchr(arg_ip_value_start,r->args.data+r->args.len,separator);
+	arg_ip_value.data = arg_ip_value_start;
+	arg_ip_value.len = arg_ip_value_end - arg_ip_value_start;
 
-	ngx_http_access_dynamic_main_conf_t *main_conf = ngx_http_get_module_main_conf(r,ngx_http_access_dynamic_module);
-//	ngx_log_error(NGX_LOG_ERR,r->connection->log,0,"ngx_http_access_dynamic_module ctx_index:%ui,main_conf:%p,main_conf->shm_zone:%p",ngx_http_access_dynamic_module.ctx_index,main_conf,main_conf->shm_zone);
-
-	ngx_http_access_dynamic_ctx_t *ctx = (ngx_http_access_dynamic_ctx_t *)main_conf->shm_zone->data;
-	ngx_int_t lastid = ctx->shm->last_used_bucket;
+	rc = search_from_dict(ctx,&arg_ip_value);
 
 //	ngx_log_error(NGX_LOG_ERR,r->connection->log,0,"lastid:%d",lastid);
 
-	size_t size = sizeof("lastid:\r\n")+sizeof(lastid);
-	ngx_buf_t *b = ngx_create_temp_buf(r->pool,size);
+	ngx_buf_t *b = ngx_create_temp_buf(r->pool,1);
 	ngx_chain_t *out = ngx_alloc_chain_link(r->pool);
 	if(b == NULL || out == NULL){
 		return NGX_HTTP_INTERNAL_SERVER_ERROR;
 	}
 	out->buf = b;
 	out->next = NULL;
-	b->last = ngx_sprintf(b->last,"lastid:%i\r\n",lastid);
+	if(rc>=0){
+		b->last = ngx_sprintf(b->last,"1");
+	}else{
+		b->last = ngx_sprintf(b->last,"0");
+	}
 	b->last_buf = 1;
-
 	r->headers_out.status = NGX_HTTP_OK;
-
 	rc = ngx_http_send_header(r);
-	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
-	{
+	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
+	    return rc;
+	}
+
+	return ngx_http_output_filter(r, out);
+}
+
+static char *
+ngx_http_access_dynamic_del_command(ngx_conf_t *cf, ngx_command_t *cmd, void *conf){
+	ngx_http_core_loc_conf_t  *clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+	clcf->handler = ngx_http_access_dynamic_del_handler;
+	return NGX_CONF_OK;
+}
+
+static ngx_int_t
+ngx_http_access_dynamic_del_handler(ngx_http_request_t *r){
+	ngx_int_t rc;
+	ngx_str_t arg_ip_value;
+	char *arg_ip_name = "ip=";
+	char separator = '&';
+	u_char *arg_ip_value_start,*arg_ip_value_end;
+	ngx_http_access_dynamic_main_conf_t *main_conf;
+	ngx_http_access_dynamic_ctx_t *ctx;
+
+	if (!(r->method & (NGX_HTTP_GET)))	{
+	    return NGX_HTTP_NOT_ALLOWED;
+	}
+	rc = ngx_http_discard_request_body(r);
+	if (rc != NGX_OK)	{
+	    return rc;
+	}
+	ngx_str_set(&r->headers_out.content_type,"text/plain");
+	main_conf = ngx_http_get_module_main_conf(r,ngx_http_access_dynamic_module);
+	ctx = (ngx_http_access_dynamic_ctx_t *)main_conf->shm_zone->data;
+	arg_ip_value_start = ngx_strnstr((u_char *)r->args.data,arg_ip_name,r->args.len);
+	arg_ip_value_start += sizeof("ip=");
+	arg_ip_value_end = ngx_strlchr(arg_ip_value_start,r->args.data+r->args.len,separator);
+	arg_ip_value.data = arg_ip_value_start;
+	arg_ip_value.len = arg_ip_value_end - arg_ip_value_start;
+
+	rc = search_from_dict(ctx,&arg_ip_value);
+
+//	ngx_log_error(NGX_LOG_ERR,r->connection->log,0,"lastid:%d",lastid);
+
+	ngx_buf_t *b = ngx_create_temp_buf(r->pool,1);
+	ngx_chain_t *out = ngx_alloc_chain_link(r->pool);
+	if(b == NULL || out == NULL){
+		return NGX_HTTP_INTERNAL_SERVER_ERROR;
+	}
+	out->buf = b;
+	out->next = NULL;
+	if(rc>=0){
+		b->last = ngx_sprintf(b->last,"1");
+	}else{
+		b->last = ngx_sprintf(b->last,"0");
+	}
+	b->last_buf = 1;
+	r->headers_out.status = NGX_HTTP_OK;
+	rc = ngx_http_send_header(r);
+	if (rc == NGX_ERROR || rc > NGX_OK || r->header_only){
 	    return rc;
 	}
 
